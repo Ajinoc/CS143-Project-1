84fdbe671a938ce303e3307829bf15cd0683ddc7
Kaitlyn Cason <kaitlyncason@wifi-131-179-47-180.host.ucla.edu>
2016-04-08 11:33:38 -0700

    Checked second query.

--- queries.sql
+++ queries.sql
@@ -1,26 +1,33 @@
---names <firstname> <lastname> of all actors in the movie 'Die Another Day'
+/*names <firstname> <lastname> of all actors in the movie 'Die Another Day'*/
 SELECT CONCAT(first, ' ', last) FROM Actor 
 INNER JOIN MovieActor ON Actor.id=MovieActor.aid 
 INNER JOIN Movie ON MovieActor.mid=Movie.id 
 WHERE title='Die Another Day';
 
---count of all actors who acted in multiple movies
+/*count of all actors who acted in multiple movies*/
 SELECT COUNT(DISTINCT ma1.aid) FROM MovieActor ma1 
 INNER JOIN MovieActor ma2 ON ma1.aid=ma2.aid 
 WHERE ma1.mid<>ma2.mid;
 
---title of movies that sold over 1,000,000 tickets
+/*count of all actors who acted in multiple movies (TRY 2)*/
+SELECT COUNT(DISTINCT ma1.aid) FROM MovieActor ma1 
+WHERE EXISTS 
+(SELECT * FROM MovieActor ma2 
+	WHERE ma1.mid <> ma2.mid AND ma1.aid = ma2.aid);
+
+/*title of movies that sold over 1,000,000 tickets*/
 SELECT title FROM Movie 
 INNER JOIN Sales ON Movie.id=Sales.mid 
 WHERE ticketsSold>1000000;
 
---title of movies that have below a 25% approval rating on Rotten Tomatoes, 
---but over 75% approval rating on IMDB
+/*title of movies that have below a 25% approval rating on Rotten Tomatoes, 
+	but over 75% approval rating on IMDB*/
 SELECT title FROM Movie  
 INNER JOIN MovieRating ON Movie.id=MovieRating.mid  
 WHERE rot<25 AND imdb>75;
 
---names <firstname> <lastname> of all actors who appeared in movies as 'Himself' or 'Herself'
+/*names <firstname> <lastname> of all actors who 
+	appeared in movies as 'Himself' or 'Herself'*/
 SELECT DISTINCT CONCAT(first, ' ', last) FROM Actor  
 INNER JOIN MovieActor ON Actor.id=MovieActor.aid  
 WHERE role='Himself' OR role='Herself';
