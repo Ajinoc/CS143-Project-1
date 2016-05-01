54d7035f0db223c1e0dbc7d08f4159be322eab9b
Kaitlyn Cason <kaitlyncason@s-128-97-87-223.resnet.ucla.edu>
2016-05-01 05:08:49 -0700

    Finished I1 minus some checks.

---
+++ P1B/www/I1.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+	<head>
+		<title>add actor / director</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>	
+	<body>
+		<h3>Add new actor/director: </h3>
+
+		<form action="<?php $_PHP_SELF ?>" method="GET">
+			
+			Identity:	<input type="radio" name="identity" value="Actor"checked="true">Actor
+						<input type="radio" name="identity" value="Director">Director<br/>
+			<hr/>
+			First Name:	<input type="text" name="first" maxlength="20"><br/>
+			Last Name:	<input type="text" name="last" maxlength="20"><br/>
+			Sex:		<input type="radio" name="sex" value="Male" checked="true">Male
+						<input type="radio" name="sex" value="Female">Female<br/>
+						
+			Date of Birth (yyyy-mm-dd):	<input type="text" name="dob"><br/>
+			Date of Death (yyyy-mm-dd):	<input type="text" name="dod"> (If still alive, leave blank.)<br/><hr/>
+			<input type="submit" name="submit" value="Add to the database!"/>
+		</form>
+
+
+        <?php
+
+        	/* Gather info */
+        	$fname = $_GET["first"];
+        	$lname = $_GET["last"];
+        	$sex = $_GET["sex"];
+        	$dob = $_GET["dob"];
+        	$dod = $_GET["dod"];
+        	$identity = $_GET["identity"];
+
+
+        	/* Check for empty fields */
+        	if(isset($_GET['submit']) && ($fname == "" || $lname == ""))
+        		print "Go back and check first or last name fields please. <br />";
+        	if(isset($_GET['submit']) && $dob == "")
+        		print "Go back and check date of birth field please. <br />";
+
+        	/* Proceed to add to database */
+        	if($fname != "" && $lname != "" && $dob != ""){
+        		$db_connection = mysql_connect("localhost", "cs143", "");
+
+	        	if(!$db_connection) {
+    	        	$errmsg = mysql_error($db_connection);
+        	    	print "Connection failed: $errmsg <br />";
+            		exit(1);
+        		}
+
+        		mysql_select_db("CS143", $db_connection);
+
+        		/* Grab max person ID */
+    	    	$query = "SELECT MAX(id)+1 FROM MaxPersonID";
+	            $rs = mysql_query($query, $db_connection);
+        	    $pid = mysql_fetch_array($rs);
+                print "$pid[0] <br />";
+
+                /* Account for empty dod field */
+                $dod = !empty($dod) ? "'$dod'" : "NULL";
+
+                /* Generate query format */
+            	if($identity == "Actor") {
+            		$query_insert = "INSERT INTO Actor(id,first,last,sex,dob,dod) VALUES ('$pid[0]','$fname','$lname','$sex','$dob', $dod)";
+        		}
+        
+        		if($identity == "Director") {
+            		$query_insert = "INSERT INTO Director(id,first,last,dob,dod) VALUES ('$pid','$fname','$lname','$dob',$dod)";
+        		}
+        		$query_update = "UPDATE MaxPersonID SET id = $pid[0]";
+
+        		/* Perform MySQL INSERT/UPDATE */
+        		$ins_q = mysql_query($query_insert, $db_connection) or
+            	print(mysql_error());
+           
+           		if (!$ins_q )
+           			print "OW";
+            	if($ins_q <> false){
+                	$upd_q = mysql_query($query_update, $db_connection) or
+                	print(mysql_error());
+           		}
+            	if($ins_q && $upd_q){
+                	print "Successfully added. <br />";
+            	}
+        	}   
+        	mysql_close($db_connection);
+        ?>
+    </body>
+</html>
---
+++ P1B/www/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<head>
+    <title>CS143 Project 1B - Movie Database -</title>
+    <link rel="stylesheet" href="style.css" />
+    <base href=""/>
+<body>
+
+    <h1>Kaitlyn Cason and Adam Jones's Movie Database</h1>
+    <iframe src="I1.php" name="iframe" id="iframe" style="overflow: hidden; height: 100%; width: 100%; position: absolute;" align=right frameborder="0" height=100% width=80% margin-left:0></iframe>
+
+    <div id="menu">
+
+        <p><b> Add: </b></p>
+        <ul>
+            <li><a href="I1.php" target="iframe">Actor/Director Information</a></li>
+            <li><a href="I2.php" target="iframe">Movie Information</a></li>
+            <li><a href="I3.php" target="iframe">Movie Comment</a></li>
+            <li><a href="I4.php" target="iframe">Actor to Movie Relation</a></li>
+            <li><a href="I5.php" target="iframe">Director to Movie Relation</a></li>
+        </ul>
+
+        <p><b> Search: </b></p>
+        <ul>
+            <li><a href="S1.php" target="iframe">Search for Actor/Actress/Movie</a></li>
+        </ul>
+
+        <p><b> Browse: </b></p>
+        <ul>
+            <li><a href="B1.php?param1=52794" target="iframe">Browse Actor Information</a></li>
+            <li><a href="B2.php?param1=2632" target="iframe">Browse Movie Information</a></li>
+        </ul>
+
+    </div>
+
+</head>
+</body>
+</html>
+
+
